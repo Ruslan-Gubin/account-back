84591e52000fb08d1566f5a1eca5b3bbf60954a4
ruslan1986 <gubin_ruslan@rambler.ru>
2023-12-19 22:30:06 +0300

    change

--- dist/index.js
+++ dist/index.js
@@ -20,6 +20,21 @@ app.use(express.json({ limit: '50mb' }));
     }
 })();
 app.use(routes.authRouter);
+app.get('/about', (req, res) => res.send(`
+<h1>Account back</h1>
+<div>Hello World</div>
+<ul class='ul'>
+<li>one</li>
+<li>two</li>
+<li>three</li>
+</ul>
+<script>
+const elem = document.querySelector('.ul')
+elem.addEventListener('click', () => {
+  console.log('Hello')
+})
+</script>
+`));
 app.listen(process.env['PORT'] || 4444, () => {
     logger.connectSuccess(`Listening port ${process.env['PORT'] || 4444}`);
 });
