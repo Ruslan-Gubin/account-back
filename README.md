7038ad9cd0e478244c1c5996973a24233a26649b
ruslan1986 <gubin_ruslan@rambler.ru>
2023-12-19 23:21:39 +0300

    change2

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # account-back
 
-
+"dev": "npx nodemon",
+    "build": "tsc -w",
+    "test": "jest -c ./jest.unit.json",
+    "lint": "npx eslint ./src",
+    "format": "npx eslint ./src --fix",
+    "build": "tsc",
+    "start": "node ./dist",
---
+++ dist/types/index.js
@@ -0,0 +1,6 @@
+export * from './IRequestRespons/index.js';
+export * from './cloudinary/index.js';
+export * from './Document/index.js';
+export * from './user/index.js';
+export * from './HandlersType.js';
+export * from './IReturnErrorObj.js';
